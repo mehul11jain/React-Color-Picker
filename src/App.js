2fb35a1502dc92d7f32b1f62f60211c912a7e8a3
MEHUL JAIN <mehulj@cse.iitb.ac.in>
2020-06-13 18:13:13 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./colorsHelpers";
 import {Route,Switch} from 'react-router-dom';
 class App extends Component {
+  findPalette(id){
+    return seedColors.find((palette)=>{
+      return palette.id === id
+    })
+  }
   render(){
     console.log(generatePalette(seedColors[4]));
-    return (       
+    return (
       <Switch>
-        <Route exact path="/"  render={()=><h1>Palette List Here</h1>} />
-        <Route exact path="/palette/:id" render={()=><h1>Individual palette</h1>}/>
+        <Route exact path="/" render={() => <h1>Palette List Here</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+          )}
+        />
       </Switch>
-      // <div className = "App" >  
-        // <Palette palette={generatePalette(seedColors[4])}/>
+      // <div className = "App" >
+      // <Palette palette={generatePalette(seedColors[4])}/>
       // </div>
-
     );
   }
 }
